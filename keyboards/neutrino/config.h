5905f945203f3f2ca34b53a9293a7f58b8f768b9
Clement Lu <clement.lu@capitalone.com>
2016-11-07 09:44:37 -0500

    update LED count

--- keyboards/neutrino/config.h
+++ keyboards/neutrino/config.h
@@ -72,7 +72,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN D5     // The pin your RGB strip is wired to
 //#define RGBLIGHT_TIMER    // Require for fancier stuff (not compatible with audio)
-#define RGBLED_NUM 16     // Number of LEDs
+#define RGBLED_NUM 19     // Number of LEDs
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
